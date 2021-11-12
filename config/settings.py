d45be2bc5b125a1807d4243d2b2ecc1986cb7ed8
DESKTOP-DOGVD1T\BIT <t781797@gmail.com>
2021-11-12 09:45:07 +0900

    1차 수정(11/12)

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mypage.apps.MypageConfig',
     'common.apps.CommonConfig',
     'maps.apps.MainConfig',
     'pybo.apps.PyboConfig',
