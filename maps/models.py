45dc571e6724b8486480f52570657ea3e8c10682
sanguk <cloryun@gmail.com>
2021-11-26 15:26:01 +0900

    1126

--- maps/models.py
+++ maps/models.py
@@ -39,6 +39,21 @@ class address(models.Model):
 
 
 
+class district(models.Model):
+    id = models.CharField(max_length=5, primary_key=True)
+    city = models.CharField(max_length=10)
+    addr1 = models.CharField(max_length=20)
+    addr2 = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'district2'
+        managed = False
+
+    def __str__(self):
+        return self.no
+
+
+
 
 
 
