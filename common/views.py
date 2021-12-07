2785756493b13d96b48c2f106eb51e8e24425d40
DESKTOP-DOGVD1T\BIT <t781797@gmail.com>
2021-12-07 14:19:37 +0900

    에러페이지 생성

--- common/views.py
+++ common/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
+
 from .forms import UserForm
 
 
@@ -20,3 +21,25 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
+
+
+def page_not_found(request, excpetions):
+    """
+    404 Page not found
+    """
+    return render(request, 'common/404.html', status=404)
+    # response = render_to_response('common/404.html', {}, context_instance=RequestContext(request))
+    # response = render(RequestContext(request), 'common/404.html', {})
+    # response.status_code = 404
+    # return response
+
+
+def server_error(request):
+    """
+    500 Internal Server Error
+    """
+    return render(request, 'common/500.html', status=500)
+    # response = render_to_response('common/500.html', {}, context_instance=RequestContext(request))
+    # response = render(RequestContext(request), 'common/500.html', {})
+    # response.status_code = 500
+    # return response
