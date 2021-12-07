9feca6227c348e9fc02e05ada6d143a8b199d6bd
DESKTOP-DOGVD1T\BIT <t781797@gmail.com>
2021-12-07 11:39:20 +0900

    마이페이지-후기목록추가&프론트 수정

--- mypage/views.py
+++ mypage/views.py
@@ -1,52 +1,51 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q, Count
 from pybo.models import Question, Answer
 from maps.models import *
+from board.models import *
 
 # Create your views here.
 
 # 자신의 질문목록출력
+@login_required(login_url="common:login")
 def index(request):
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
-    kw = request.GET.get('kw', '')  # 검색어
-    so = request.GET.get('so', 'recent')  # 정렬기준
+    so = request.GET.get('so', 'question_list')  # 정렬기준
     user = request.user
 
     list = Question.objects.order_by('create_date')
-    list2 = data.objects.filter(mark=user.id)
+
 
     # 정렬
     if so == 'question_list':
-        list = Question.objects.order_by('create_date')
+        list = Question.objects.order_by('-create_date')
 
 
 
         list = list.filter(
-            Q(author__username__icontains=request.user)
+            Q(author__username__icontains=user)
         )
 
-        # 조회
-        if kw:
-            list = list.filter(
-                Q(subject__icontains=kw) |  # 제목검색
-                Q(content__icontains=kw) |  # 내용검색
-                Q(author__username__icontains=kw) |  # 질문 글쓴이검색
-                Q(answer__author__username__icontains=kw) |  # 답변 글쓴이검색
-                Q(answer__content__icontains=kw)  # 답변 내용검색
-            ).distinct()
-
-
     elif so == 'mark_list':
-        list = list2
+        mark_list = data.objects.filter(mark=user.id)
+        list = mark_list
+
+    elif so == 'comment_list':
+        comment_list = ShopComment.objects.order_by('-create_date').filter(
+            Q(author__username__icontains=user)
+        )
+        list = comment_list
+
 
     # 페이징처리
-    paginator = Paginator(list, 10)  # 페이지당 10개씩 보여주기
+    paginator = Paginator(list, 9)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'list': page_obj, 'page': page, 'kw': kw, 'so': so}
+    context = {'list': page_obj, 'page': page, 'so': so}
     return render(request, 'mypage/mypage.html', context)
 
 
@@ -62,7 +61,7 @@ def save_delete(request, data_id):
 
     list = data.objects.filter(mark=user.id)
 
-    paginator = Paginator(list, 10)  # 페이지당 10개씩 보여주기
+    paginator = Paginator(list, 9)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
     return render(request, 'mypage/mypage.html',{'so': 'mark_list','list':page_obj})
