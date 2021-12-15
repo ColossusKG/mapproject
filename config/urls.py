279e818d51a5a31b013b4e852b4d7e610f7a0505
sanguk <cloryun@gmail.com>
2021-12-15 16:42:01 +0900

    1215 reviewpage error fix

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from pybo.views import base_views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('common/', include('common.urls')),
     path('mypage/', include('mypage.urls')),
     path('board/', include('board.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
     #path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
 
